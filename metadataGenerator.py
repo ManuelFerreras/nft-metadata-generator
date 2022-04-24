9be3b3d13146879fcbec601f1184bcda416c84df
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2022-04-24 12:03:10 -0300

    finished

---
+++ metadataGenerator.py
@@ -0,0 +1,50 @@
+# Imports
+import json
+
+
+# Settings
+base_uri = ""
+media_extension = ".png"
+rareness = ["Obsidian", "Emerald", "Ruby", "Diamond"]
+heroes = ["Reaper", "Warrior", "Sourcerer"]
+stars = [1, 2, 3]
+amount_of_combinations = 3
+amount_of_sub_combinations = 4
+amount_of_sub_sub_combinations = 3
+counter = 0
+
+
+# Logic
+for i in range(amount_of_combinations):
+    for j in range(amount_of_sub_combinations):
+        for k in range(amount_of_sub_sub_combinations):
+            
+            base_metadata = {
+
+                "image": base_uri + str(i + 1) + str(j + 1) + str(k + 1) + media_extension,
+                "name": rareness[j] + " " + heroes[i],
+                "attributes": [
+
+                    {
+                        "trait_type": "Stars",
+                        "value": stars[k]
+                    },
+
+                    {
+                        "trait_type": "Heroe",
+                        "value": heroes[i]
+                    }
+                
+                ]
+
+            }
+
+            print(base_metadata)
+
+            jsonFile = open("./metadata/" + str(i + 1) + str(j + 1) + str(k + 1) + ".json", "w")
+            jsonFile.write(json.dumps(base_metadata))
+            jsonFile.close()
+
+            counter = counter + 1
+
+print("Generated Metadatas: " + str(counter))
